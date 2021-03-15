20c8e2a0d30aa0e0ba6897fa188e6acd9b5f822f
Paul Saxe <paul@thesaxes.net>
2021-03-15 02:39:08 -0400

    Fixed yapf error.

--- table_step/__init__.py
+++ table_step/__init__.py
@@ -16,6 +16,7 @@ from table_step.tk_table import TkTable  # noqa: F401
 
 # Handle versioneer
 from ._version import get_versions
+
 __author__ = """Paul Saxe"""
 __email__ = 'psaxe@molssi.org'
 versions = get_versions()
